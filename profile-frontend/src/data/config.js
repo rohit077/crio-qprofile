cfcf82f50d710a06467c80833a60ca14b82d68d6
rohit077 <roychowdhuryrohit0@gmail.com>
2021-01-26 19:48:20 +0530

    deployed backend

---
+++ profile-frontend/src/data/config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  name: "Rohit Roy Chowdhury",
+  greeting: "Hello 👋",
+  greetingHead: "I'm Rohit Roy Chowdhury",
+  greetingDescription: "Designer/Developer",
+  githubUrl: "https://github.com/rohit077",
+  linkedinUrl: "https://linkedin.com/in/RohitRoyChowdhury",
+  cvLink: "https://drive.google.com/file/d/17pCmw2v7Xe-INa8v31jadpmYsLaW2D6d/",
+};
